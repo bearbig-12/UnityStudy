6ac0fc1a7d4bb9aada811e3015a5f5e8e9f8f440
Hyeontae An <83248120+bearbig-12@users.noreply.github.com>
2025-10-20 21:30:35 +0900

    Pyramid Update

---
+++ My project/Assets/Scripts/20251018/Pyramid.cs
@@ -0,0 +1,134 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pyramid: MonoBehaviour
+{
+    [SerializeField] private Texture _texture;
+
+    float _rotSpeed = 40.0f;
+    float _angle = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MakePyramid();
+    }
+
+    void MakePyramid()
+    {
+        // 정점버퍼에 입력할 정점.
+        // 텍스쳐를 위한 정점 24개
+        Vector3[] vertices = new Vector3[]
+        {
+            // 앞
+            new Vector3(0.0f, 0.0f, 0.0f), // 0
+            new Vector3(0.5f, 1.0f, 0.5f),  // 1
+            new Vector3(1.0f, 0.0f, 0.0f),  // 2
+
+            // 뒤
+            new Vector3(0.0f, 0.0f, 1.0f), // 3
+            new Vector3(0.5f, 1.0f, 0.5f),  // 4
+            new Vector3(1.0f, 0.0f, 1.0f),  // 5
+
+            // 왼 
+            new Vector3(0.0f, 0.0f, 1.0f), // 6
+            new Vector3(0.5f, 1.0f, 0.5f), // 7
+            new Vector3(0.0f, 0.0f, 0.0f), // 8
+
+            // 오 
+            new Vector3(1.0f, 0.0f, 0.0f), // 9
+            new Vector3(0.5f, 1.0f, 0.5f), // 10
+            new Vector3(1.0f, 0.0f, 1.0f), // 11
+
+        
+            // 아래 (
+            new Vector3(0.0f, 0.0f, 0.0f), // 12
+            new Vector3(0.0f, 0.0f, 1.0f), // 13
+            new Vector3(1.0f, 0.0f, 1.0f), // 14
+            new Vector3(1.0f, 0.0f, 0.0f), // 15
+        };
+
+        // 인덱스 버퍼에 저장할 Data
+        int[] triangles = new int[]
+        {
+            // 앞 
+            0, 1, 2,
+
+            // 뒤 
+            5,4,3,
+            
+            // 왼 
+            6, 7, 8,
+
+            // 오 
+            9,10,11,
+
+   
+            // 아래 
+             12, 14, 13,   12, 15, 14
+
+
+        };
+
+        float h = 1f / 4f;
+        float w = 1f / 4f;
+
+        Vector2[] uvFull = new Vector2[16];
+        // 앞 - 1
+        uvFull[0] = new Vector2(w, 2 * h);
+        uvFull[1] = new Vector2(1.5f * w, 3 * h);
+        uvFull[2] = new Vector2(2 * w, 2 * h);
+
+        // 뒤 - 6
+        uvFull[3] = new Vector2(w, 0f);
+        uvFull[4] = new Vector2(0.5f * w, h);
+        uvFull[5] = new Vector2(2 * w, 0f);
+
+        // 왼 - 5
+        uvFull[6] = new Vector2(0f, 0f);
+        uvFull[7] = new Vector2(0.5f * w, h);
+        uvFull[8] = new Vector2(w, 0f);
+
+        //오 - 2
+        uvFull[9] = new Vector2(2 * w, 0f);
+        uvFull[10] = new Vector2(2.5f * w, h);
+        uvFull[11] = new Vector2(3 * w, 0f);
+
+
+        //아래 - 4
+        uvFull[12] = new Vector2( w, 3 * h);
+        uvFull[13] = new Vector2( w, 4 * h);
+        uvFull[14] = new Vector2(2 * w, 4 * h);
+        uvFull[15] = new Vector2(2 * w, 3 * h);
+
+
+
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices; // 정점버퍼에 정점 데이타 전달
+        mesh.triangles = triangles; // 인덱스버퍼에 폴리곤(삼각형)의 인덱스값을 전달
+        mesh.uv = uvFull;
+
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        Material material = new Material(Shader.Find("Standard"));
+        material.mainTexture = _texture;
+
+        GetComponent<MeshRenderer>().material = material;
+
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //_angle += Time.deltaTime * _rotSpeed;
+        //this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, _angle);
+    }
+}
